bc8b21875fb6266ff0bc932be916ea0e4cf2dbd7
rhmunna143 <rhmunna19@gmail.com>
2026-07-30 16:50:11 +0600

    feat: implement payment success and cancel pages with rental ID handling

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+export interface IFooterProps {
+}
+
+export default class Footer extends React.Component<IFooterProps> {
+  public render() {
+    return (
+      <div>
+        footer
+      </div>
+    );
+  }
+}
